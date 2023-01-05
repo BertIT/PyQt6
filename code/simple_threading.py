269dcd8b96b20dad9150bbd4bcb2d148e579562a
BertIT <52295933+BertIT@users.noreply.github.com>
2023-01-05 08:34:18 +0100

    Update simple_threading.py
    Updated the finished signal from Function() with a connection to tzhe deleteLater function.
    Added a main() function.

--- code/simple_threading.py
+++ code/simple_threading.py
@@ -30,35 +30,46 @@ class MainWindow(qtw.QWidget):
         super().__init__()
         self.setWindowTitle('Hello World!')
 
-        # Set vertical layout
-        self.setLayout(qtw.QVBoxLayout())
-        self.func = Function()
-
         # Create a label
         self.my_label = qtw.QLabel("Hello World! What's your name")
         self.start_button = qtw.QPushButton('Start me!')
         self.start_button.clicked.connect(self.run_start_button)
-
+        
+        # Set vertical layout
+        self.setLayout(qtw.QVBoxLayout())
         self.layout().addWidget(self.start_button)
         self.layout().addWidget(self.my_label)
+
+        self.func_thread = None
+        self.func = None
+
         # show app
         self.show()
-
+        
     def run_start_button(self):
-
-        self.func.log_signal.connect(self.set_log_text)
-
         self.func_thread = QtCore.QThread(self)
+        self.func = Function()
+
         self.func.moveToThread(self.func_thread)
+
         self.func_thread.started.connect(self.func.run_function)
         self.func.finished.connect(self.func_thread.quit)
+        self.func.finished.connect(self.func.deleteLater)
+        self.func_thread.finished.connect(self.func_thread.deleteLater)
+        self.func.log_signal.connect(self.set_log_text)
+
         self.func_thread.start()
 
     def set_log_text(self, log_text):
         self.my_label.setText(log_text)
 
+        
+def main():
+    app = qtw.QApplication([])
+    mw = MainWindow()
+    app.exec()
+   
 
-app = qtw.QApplication([])
-mw = MainWindow()
+if __name__ == '__main__':
+    main()
 
-app.exec()
